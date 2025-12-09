92fc532d9be9b7ae2c46b00534a53792f63053d5
ebadianh <ebadian1992@gmail.com>
2025-12-09 19:13:34 +0100

    Done!

--- obj/Debug/net8.0/trading_system.AssemblyInfo.cs
+++ obj/Debug/net8.0/trading_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trading_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a13d41c6b1aa623388182e1d2ab8e60554b464")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49a5aac797a1fc76162ac1713463835e85af937")]
 [assembly: System.Reflection.AssemblyProductAttribute("trading_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trading_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
